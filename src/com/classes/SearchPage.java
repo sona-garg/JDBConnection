86d1812ce2af0c61a7d24c39c0475709cf705544
sona-garg <sonalipranam@gmail.com>
2022-03-28 12:30:06 -0400

    Added Branch Test Code

--- src/com/classes/SearchPage.java
+++ src/com/classes/SearchPage.java
@@ -4,6 +4,7 @@ public class SearchPage {
 
 	public static void main(String[] args) {
 		System.out.println("New Class to test git from Command prompt.");
+		System.out.println("New Statement to check Branching mechanism");
 	}
 
 }
